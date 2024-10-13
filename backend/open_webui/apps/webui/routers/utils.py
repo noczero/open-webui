caa7528ee06348e1127893901c1ff7a434763264
noczero <satrya@zeroinside.id>
2024-10-13 11:10:25 +0700

    feat: PDF generator to export chat

--- backend/open_webui/apps/webui/routers/utils.py
+++ backend/open_webui/apps/webui/routers/utils.py
@@ -1,18 +1,14 @@
-from datetime import datetime
-from io import BytesIO
-from pathlib import Path
-from textwrap import dedent
 import black
 import markdown
-from fastapi.staticfiles import StaticFiles
-from xhtml2pdf import pisa
 
+from open_webui.apps.webui.models.chats import ChatTitleMessagesForm
 from open_webui.config import DATA_DIR, ENABLE_ADMIN_EXPORT
 from open_webui.constants import ERROR_MESSAGES
 from fastapi import APIRouter, Depends, HTTPException, Response, status
 from pydantic import BaseModel
 from starlette.responses import FileResponse
 from open_webui.utils.misc import get_gravatar_url
+from open_webui.utils.pdf_generator import PDFGenerator
 from open_webui.utils.utils import get_admin_user
 
 router = APIRouter()
@@ -58,83 +54,10 @@ class ChatForm(BaseModel):
 
 @router.post("/pdf")
 async def download_chat_as_pdf(
-    form_data: ChatForm,
+    form_data: ChatTitleMessagesForm,
 ):
-    html_messages = '<div>'
-
-    # Add chat messages
-    for message in form_data.messages:
-        role = message["role"]
-        content = message["content"]
-
-        if message.get('timestamp',None):
-            date_time = datetime.fromtimestamp(message['timestamp'])
-            date_str = date_time.strftime("%Y-%m-%d, %H:%M:%S")
-        else:
-            date_str = ''
-
-        model = message['model'] if role == 'assistant' else ''
-        html_content = markdown.markdown(
-            content, extensions=['pymdownx.extra']
-        )
-        html_message = f"""
-                <div class="message">
-                    <small>{date_str}</small>
-                    <div>
-                      <h2>
-                        <strong>{role.title()}</strong>
-                        <small class="text-muted">{model}</small>
-                      </h2>
-                    </div>
-                    
-                    <div class="markdown-section">
-                        {html_content}
-                    </div>
-                </div>
-            """
-
-        html_messages += html_message
-
-    html_messages += "</div>"
-
-    css_style_file = Path("./backend/open_webui/static/assets/pdf-style.css")
-
-    html_body = dedent(
-        f"""
-            <html>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <link rel="stylesheet" href="{css_style_file}">
-                <body>
-                    <div class="container"> 
-                        <div class="text-center">
-                            <h1> 
-                                {form_data.title}
-                            </h1>
-                        </div>
-                        <div>
-                            {html_messages}
-                        </div>
-                    </div>
-                </body>
-            </html>
-        """
-    )
-
-
-    pdf_buffer = BytesIO()
-    pisa_status = pisa.CreatePDF(html_body, dest=pdf_buffer)
-
-    if pisa_status.err:
-        return Response(content="Error generating PDF", status_code=500)
-
-    pdf_bytes = pdf_buffer.getvalue()
-
-    return Response(
-        content=pdf_bytes,
-        media_type="application/pdf",
-        headers={"Content-Disposition": "attachment;filename=chat.pdf"},
-    )
+    response = PDFGenerator(form_data).generate_chat_pdf()
+    return response
 
 
 @router.get("/db/download")
